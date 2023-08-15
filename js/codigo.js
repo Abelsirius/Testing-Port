42aa0f28adeb74081c9ac8ceef4ed39307268df8
Abel Hurtado Sandoval <83846290+Abelsirius@users.noreply.github.com>
2023-08-14 21:09:42 -0500

    Add files via upload

--- js/codigo.js
+++ js/codigo.js
@@ -238,3 +238,84 @@ const sendMensaje = (e) =>{
 
 formWhatsApp.addEventListener('submit',sendMensaje)
 
+
+/* ------- BAR NAVIGATOR ----------*/
+
+
+let nav_bar_links = document.querySelectorAll(".link_nav");
+
+
+const currentNavBar = (e) =>{
+
+if(!e.target.classList.contains("active")){
+ console.log("no tiene")
+   nav_bar_links.forEach((item)=> item.firstChild.classList.remove("active"))
+   e.target.classList.add("active");
+}
+
+}
+
+
+nav_bar_links.forEach((item)=> item.addEventListener("click",currentNavBar))
+
+
+
+
+
+let el = document.querySelector('.profile_img')
+
+/* Get the height and width of the element */
+const height = el.clientHeight
+const width = el.clientWidth
+
+/*
+  * Add a listener for mousemove event
+  * Which will trigger function 'handleMove'
+  * On mousemove
+  */
+el.addEventListener('mousemove', handleMove)
+
+/* Define function a */
+function handleMove(e) {
+  /*
+    * Get position of mouse cursor
+    * With respect to the element
+    * On mouseover
+    */
+  /* Store the x position */
+  const xVal = e.layerX
+  /* Store the y position */
+  const yVal = e.layerY
+  
+  /*
+    * Calculate rotation valuee along the Y-axis
+    * Here the multiplier 20 is to
+    * Control the rotation
+    * You can change the value and see the results
+    */
+  const yRotation = 25 * ((xVal - width / 2) / width)
+  
+  /* Calculate the rotation along the X-axis */
+  const xRotation = 25 * ((yVal - height / 2) / height)
+  
+  /* Generate string for CSS transform property */
+  const string = 'perspective(500px) scale(1.1) rotateX(' + xRotation + 'deg) rotateY(' + yRotation + 'deg)'
+  
+  /* Apply the calculated transformation */
+  el.style.transform = string
+}
+
+/* Add listener for mouseout event, remove the rotation */
+el.addEventListener('mouseout', function() {
+  el.style.transform = 'perspective(500px) scale(1) rotateX(0) rotateY(0)'
+})
+
+/* Add listener for mousedown event, to simulate click */
+el.addEventListener('mousedown', function() {
+  el.style.transform = 'perspective(500px) scale(0.9) rotateX(0) rotateY(0)'
+})
+
+/* Add listener for mouseup, simulate release of mouse click */
+el.addEventListener('mouseup', function() {
+  el.style.transform = 'perspective(500px) scale(1.1) rotateX(0) rotateY(0)'
+})
